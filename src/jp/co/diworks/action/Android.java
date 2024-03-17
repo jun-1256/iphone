03c13f81077eace9e4d400f68eee01025655cf9d
jun <jun@localhost>
2024-03-18 01:16:37 +0900

    2024/03/18

---
+++ src/jp/co/diworks/action/Android.java
@@ -0,0 +1,15 @@
+package jp.co.diworks.action;
+
+public class Android {
+
+	public static void main(String[] args) {
+		SmartPhone  android = new SmartPhone();
+		android.music();
+		android.run();
+		android.walk();
+		android.summer();
+		android.winter();
+
+	}
+
+}
---
+++ src/jp/co/diworks/action/SmartPhone.java
@@ -0,0 +1,45 @@
+package jp.co.diworks.action;
+
+public class SmartPhone extends Mp3Player {
+	public void play() {
+		System.out.println("再生（ハイレゾ機能）");
+	}
+	public void stop() {
+		System.out.println("停止（ハイレゾ機能）");
+	}
+	public void next() {
+		System.out.println("次へ（ハイレゾ機能）");
+	}
+	public void back() {
+		System.out.println("戻る（ハイレゾ機能）");
+	}
+	public void call() {
+		System.out.println("電話");
+	}
+	public void mail() {
+		System.out.println("メール");
+	}
+	public void phone() {
+		System.out.println("写真");
+	}
+	public void internet() {
+		System.out.println("インターネット");
+	}
+	
+	public void music() {
+		System.out.println("音楽");
+	}
+	public void run() {
+		System.out.println("走る");
+	}
+	public void walk() {
+		System.out.println("歩く");
+	}
+	public void summer() {
+		System.out.println("夏");
+	}
+	public void winter() {
+		System.out.println("冬");
+	}
+
+}
